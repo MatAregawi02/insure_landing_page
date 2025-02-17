e69358a9444155ea76ae3dfa70bae96f34d04146
MatAregawi02 <mataregwi@gmail.com>
2025-02-17 13:00:26 -0500

    Made header responsive for mobile screens

--- js/app.js
+++ js/app.js
@@ -2,7 +2,35 @@ let header = document.querySelector("header");
 let navigation = document.querySelector("nav");
 let menu = document.querySelector("ul");
 let hamburgerIcon = document.querySelector(".hamburger-icon");
+let crossIcon = document.querySelector(".cross-icon");
 let mobileWidth = 600;
-let screenWidth = document.documentElement.clientWidth;
 
 
+
+
+hamburgerIcon.addEventListener("click", () => {
+    navigation.classList.toggle("hidden");
+    hamburgerIcon.style.display = "none";
+    crossIcon.style.display = "inline-block";
+})
+crossIcon.addEventListener("click",()=>{
+    navigation.classList.toggle("hidden");
+    crossIcon.style.display = "none";
+    hamburgerIcon.style.display = "inline-block";
+});
+
+function updateNavVisibility(){
+    let screenWidth = document.documentElement.clientWidth; //should be defined in the function
+    if (screenWidth > 600){
+        navigation.classList.remove("hidden");
+    }
+    else{
+        navigation.classList.add("hidden");
+        crossIcon.style.display = "none";
+        hamburgerIcon.style.display = "inline-block";
+    }
+}
+
+window.addEventListener('resize', updateNavVisibility);
+
+updateNavVisibility(); //upon reload
